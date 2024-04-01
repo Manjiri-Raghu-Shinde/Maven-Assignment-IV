eeead112a62e32a59655088b2e9e0a653f376a7c
Manjiri-Raghu-Shinde <manjiri.r.shinde@gmail.com>
2024-04-01 19:38:47 +0530

    Add files

---
+++ src/main/java/SeleniumAssignment/AssignmentIV_CI_CD/App.java
@@ -0,0 +1,9 @@
+package SeleniumAssignment.AssignmentIV_CI_CD;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/SeleniumAssignment/AssignmentIV_CI_CD/AppTest.java
@@ -0,0 +1,11 @@
+package SeleniumAssignment.AssignmentIV_CI_CD;
+
+import org.testng.annotations.Test;
+
+public class AppTest{
+	
+	@Test
+	public void SampleTest() {
+		System.out.println("this is a sample test case");
+	}
+}
